30ac3b564377d4fecbac0b4f692a741b0ecce7bd
DaCh0sen1 <darcynsmall@gmail.com>
2015-11-19 01:04:58 +0000

    Clean ups, add string replacement for database.php etc

--- install-scripts/debian8-MISP-install-script-v0.01.sh
+++ install-scripts/debian8-MISP-install-script-v0.01.sh
@@ -27,6 +27,8 @@ MISPDIRLOC="/var/www/${MISPDIRNAME}"
 MISPREPO="https://github.com/MISP/MISP.git"
 MISPVERSION="2.4-beta"
 MISPDBHOST="localhost"
+MISPDBPORT="3306"
+MISPDBPREFIX=""
 
 # CYBOX info
 CYBOX="python-cybox"
@@ -43,18 +45,15 @@ STIXREPO="https://github.com/STIXProject/python-stix.git"
 #Composer URI
 COMPOSERURI="https://getcomposer.org/installer"
 
-#MYSQL DB info
-#MYSQLPASS="misp"
-#MISPDBUSER="misp"
-#MISPDB="misp"
-#MISPDBPASS="${MYSQLPASS}"
-
 #MySQL Variables
 MYSQL_VER="mysql-server-5.5"
 MYSQL_ROOT_PASS=$(apg -q -a 1 -n 1 -m 15 -x 15 -M NCL)
 MYSQL_USER="misp"
 MYSQL_USER_DB="misp"
 MYSQL_USER_PASS=$(apg -q -a 1 -n 1 -m 15 -x 15 -M NCL)
+
+# MISP config.php values
+MISPSALT=$(apg -q -a 1 -n 1 -m 32 -x 32 -M NCLS)
 
 # Set Apt-Cacher-NG Proxy
 APTPROXY="http://192.168.1.91:3142/"
@@ -105,13 +104,8 @@ php composer.phar install
 cd -
 
 # CakeResque normally uses phpredis to connect to redis, but it has a (buggy) fallback connector through Redisent. It is highly advised to install phpredis
-#pecl install redis
+pecl install redis
 apt-get install -y php5-redis
-# Note that the php5-redis package in Debian stable (wheezy) only exists in the backports repository: http://backports.debian.org/Instructions/
-# After installing it, enable it in your php.ini file
-#vim /etc/php5/apache2/php.ini
-# add the following line:
-#extension=redis.so
 
 # Restart Apache
 apachectl restart
@@ -124,40 +118,12 @@ cp -fva ${MISPDIRLOC}/INSTALL/setup/config.php ${MISPDIRLOC}/app/Plugin/CakeResq
 
 # Check if the permissions are set correctly using the following commands as root:
 chown -R www-data:www-data /var/www/MISP
-#chmod -R 750 /var/www/MISP
-#chmod -R g+ws /var/www/MISP/app/tmp
-#chmod -R g+ws /var/www/MISP/app/files
-#chmod -R g+ws /var/www/MISP/app/files/scripts/tmp
-
-#BETTER!!
 find /var/www/MISP -type d -exec chmod 2750 {} \+
 find /var/www/MISP -type f -exec chmod 2640 {} \+
 find /var/www/MISP/app/{tmp,files} -type d -exec chmod 2770 {} \+
 find /var/www/MISP/app/{tmp,files} -type f -exec chmod 660 {} \+
 find /var/www/MISP/app/Console -type f -exec chmod 770 {} \+
 
-################################
-## CREATE DB ETC
-#EXPECTED_ARGS=3
-#E_BADARGS=65
-#MYSQL=`which mysql`
-#
-#Q1="CREATE DATABASE IF NOT EXISTS $1;"
-#Q2="GRANT USAGE ON *.* TO $2@localhost IDENTIFIED BY '$3';"
-#Q3="GRANT ALL PRIVILEGES ON $1.* TO $2@localhost;"
-#Q4="FLUSH PRIVILEGES;"
-#SQL="${Q1}${Q2}${Q3}${Q4}"
-#
-#if [ $# -ne $EXPECTED_ARGS ]
-#then
-#  echo "Usage: $0 misp misp misp"
-#  exit $E_BADARGS
-#fi
-#
-#$MYSQL -umisp -pmisp -e "$SQL"
-#
-#mysql -u misp -p${MISPDBPASS} misp < ${MISPDIRLOC}/INSTALL/MYSQL.sql
-#################################
 
 export DEBIAN_FRONTEND=noninteractive
 #Setup debconf for mysql-server
@@ -209,35 +175,41 @@ cp -va database.default.php database.php
 cp -va core.default.php core.php
 cp -va config.default.php config.php
 
-# Configure the fields in the newly created files:
-# database.php : login, port, password, database
-# bootstrap.php: uncomment the last 3 lines to enable the background workers (see below)
-# CakePlugin::loadAll(array('CakeResque' => array('bootstrap' => true)));
+echo -e "[+] Rewriting values for database connection in ${MISPDIRLOC}/app/Config/database.php...\n"
+sed -i "s/'host' => 'localhost',/'host' => '${MISPDBHOST}',/g"  app/Config/database.php
+sed -i "s/'login' => 'db login',/'login' => '${MYSQL_USER}',/g"  app/Config/database.php
+sed -i "s/'port' => '3306',/'port' => '${MISPDBPORT}',/g"  app/Config/database.php
+sed -i "s/'password' => 'db password',/'password' => '${MYSQL_USER_PASS}',/g"  app/Config/database.php
+sed -i "s/'database' => 'misp',/'database' => '${MYSQL_USER_DB}',/g"  app/Config/database.php
+sed -i "s/'prefix' => '',/'prefix' => '${MISPDBPREFIX}',/g"  app/Config/database.php
 
-# To enable the background workers, if you have installed the package required for it in 4/, uncomment the following lines:
-# in core.php (if you have just recently updated MISP, just add this line at the end of the file):
-# require_once dirname(__DIR__) . '/Vendor/autoload.php';
+echo -e "[+] Loading CakePlugin by appending to ${MISPDIRLOC}/app/Config/bootstrap.php ...\n"
+cat <<LOADCAKERESQ >> ${MISPDIRLOC}/app/Config/bootstrap.php
+CakePlugin::loadAll(array(
+        'CakeResque' => array('bootstrap' => true)
+));
 
-# Important! Change the salt key in /var/www/MISP/app/Config/config.php
-# The salt key must be an at least 32 byte long string.
-# The admin user account will be generated on the first login, make sure that the salt is changed before you create that user
-# If you forget to do this step, and you are still dealing with a fresh installation, just alter the salt,
-# delete the user from mysql and log in again using the default admin credentials (admin@admin.test / admin)
+LOADCAKERESQ
 
-#Add this line after in Config/bootstrap.php
-#After
-# CakePlugin::loadAll(array('CakeResque' => array('bootstrap' => true)));
-#Configure::write('MISP.background_jobs', true);
+echo -e "[+] Appending autoload to ${MISPDIRLOC}/app/Config/core.php ...\n"
+cat <<VENDOR >> ${MISPDIRLOC}/app/Config/core.php
+require_once dirname(__DIR__) . '/Vendor/autoload.php';
 
+VENDOR
 
-#CHECK out replacing this with sed
-#'baseurl' => '192.168.1.30',
+echo -e "[+] Appending background_jobs to ${MISPDIRLOC}/app/Config/bootstrap.php ...\n"
+cat <<BACKGROUND >> ${MISPDIRLOC}/app/Config/bootstrap.php
+Configure::write('MISP.background_jobs', true);
+
+BACKGROUND
+
+echo -e "[+] Rewriting values in config.php ...\n"
+sed -i "s/'salt' => 'Rooraenietu8Eeyo<Qu2eeNfterd-dd+',/'salt' => '${MISPSALT}',/g"  app/Config/config.php
 
 # and make sure the file permissions are still OK
 chown -R www-data:www-data ${MISPDIRLOC}/app/Config
 find ${MISPDIRLOC}/app/Config -type d -exec chmod 2750 {} \+
 find ${MISPDIRLOC}/app/Config -type f -exec chmod 2640 {} \+
-
 
 # Generate a GPG encryption key.
 #mkdir ${MISPDIRLOC}/.gnupg
@@ -254,69 +226,27 @@ find ${MISPDIRLOC}/app/Config -type f -exec chmod 2640 {} \+
 #sudo -u www-data gpg --homedir /var/www/MISP/.gnupg --export --armor YOUR-EMAIL > /var/www/MISP/app/webroot/gpg.asc
 
 # To make the background workers start on boot
-#chmod +x /var/www/MISP/app/Console/worker/start.sh
-#sudo vim /etc/rc.local
-# Add the following line before the last line (exit 0). Make sure that you replace www-data with your apache user:
-#sed -i "/exit 0/i\su www-data -c 'bash ${MISPDIRLOC}/app/Console/worker/start.sh'" /etc/rc.local
-#
-#
-#pip install pyzmq
-# Now log in using the webinterface:
-# The default user/pass = admin@admin.test/admin
+chmod +x /var/www/MISP/app/Console/worker/start.sh
+sed -i "/exit 0/i\su www-data -c 'bash ${MISPDIRLOC}/app/Console/worker/start.sh'" /etc/rc.local
 
-# Using the server settings tool in the admin interface (Administration -> Server Settings), set MISP up to your preference
-# It is especially vital that no critical issues remain!
-# start the workers by navigating to the workers tab and clicking restart all workers
+echo -e "[+] Install pyzmq using 'pip'...\n"
+pip install pyzmq
 
-#Don't forget to change the email, password and authentication key after installation.
-
-#Things to change in app/Config/config.php
-#'email' => 'email@address.com',
-#'baseurl' => '192.168.1.30',
-#    'email' => 'email@address.com',
-#    'contact' => 'email@address.com',
-#  'MISP' =>
-#  array (
-#    'baseurl' => '192.168.1.30',
-#    'footerpart1' => 'Powered by MISP',
-#    'footerpart2' => '&copy; Belgian Defense CERT & NCIRC',
-#    'org' => 'ORGNAME',
-#    'showorg' => true,
-#    'background_jobs' => true,
-#    'cached_attachments' => false,
-#    'email' => 'email@address.com',
-#    'contact' => 'email@address.com',
-#    'cveurl' => 'http://web.nvd.nist.gov/view/vuln/detail?vulnId=',
-#    'disablerestalert' => false,
-#    'default_event_distribution' => '0',
-#    'default_attribute_distribution' => 'event',
-#    'tagging' => true,
-#    'full_tags_on_event_index' => true,
-#    'footer_logo' => '',
-#    'take_ownership_xml_import' => false,
-#    'unpublishedprivate' => false,
-#  ),
-##
-##  'GnuPG' =>
-##  array (
-##    'onlyencrypted' => false,
-##    'email' => '',
-##    'homedir' => '',
-##    'password' => '',
-##    'bodyonlyencrypted' => false,
-##  ),
-#
-######
-
-#Send all necessary credential to a text file located whereever you
-#specified in CREDSFILE="/path/to/credentials/file"
+# Send all necessary credential to a text file located where ever you
+# specified in CREDSFILE="/path/to/credentials/file"
 cat <<LEMP-creds > $CREDSFILE
-#Operating System:              Debian $OS_VER
-MySQL installed version:       $MYSQL_VER
-MySQL root password:           $MYSQL_ROOT_PASS
-MySQL user:                    $MYSQL_USER
-MySQL database:                $MYSQL_USER_DB
-MySQL user password:           $MYSQL_USER_PASS
+Operating System:		Debian $OS_VER
+MySQL installed version:	$MYSQL_VER
+MySQL root password:		$MYSQL_ROOT_PASS
+MySQL user:			$MYSQL_USER
+MySQL database:			$MYSQL_USER_DB
+MySQL user password:		$MYSQL_USER_PASS
+MISP Version:			$MISPVERSION
+MISP Database Host:		$MISPDBHOST
+MISP Database Port:		$MISPDBPORT
+MISP Database Prefix:		$MISPDBPREFIX
+MISP admin user:		admin@admin.test
+MISP ddmin password:		admin
 
 LEMP-creds
 
